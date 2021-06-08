d9b875113d5dfaac95d44e938349afeda213dc37
Atikocak <atilimkocak@gmail.com>
2021-06-08 19:57:43 +0300

    Chrome Driver Testing

---
+++ main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+driverPath = "C:\Users\Atilim\Documents\My Projects\My Github\AW-Bot\chromedriver.exe"
+driver = webdriver.Chrome(driverPath)
+
+driver.get("https://play.alienworlds.io")
